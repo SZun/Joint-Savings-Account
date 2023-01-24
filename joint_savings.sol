f0311827350af03cc298bb60845d732fa5b23fbc
Sam Zun <zunsamuel97@gmail.com>
2023-01-23 23:22:33 -0600

    completed the deposit and setAccounts methods

--- joint_savings.sol
+++ joint_savings.sol
@@ -73,7 +73,7 @@ contract JointSavings {
         /*
         Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
         */
-        // YOUR CODE HERE!
+        contractBalance = address(this).balance;
     }
 
     /*
@@ -82,7 +82,8 @@ contract JointSavings {
     function setAccounts(address payable account1, address payable account2) public{
 
         // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
-        // YOUR CODE HERE!
+        accountOne = account1;
+        accountTwo = account2;
     }
 
     /*
